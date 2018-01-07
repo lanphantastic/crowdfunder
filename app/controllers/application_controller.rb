e41e301fbbd62110e50b6e44aadf383d79245fde
Lan Phan <lanc_phan@hotmail.com>
2018-01-07 14:01:27 -0500

    add before_action on project controller to check if the user has login with the help of the require_login from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def not_authenticated
+    redirect_to login_path, alert: "Please login first"
+  end
+
+  def require_login
+    unless current_user
+      not_authenticated
+    end
+  end
+
+  def is_project_owner
+    current_user == @project.user
+  end
+  helper_method :is_project_owner
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,53 @@
 class ProjectsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+  
+  def index
+    @projects = Project.order(:end_date)
+
+    if params[:category_id]
+      @projects = @projects.where(category_id: params[:category_id])
+    end
+
+    @projects = @projects.active if params[:status] == 'active'
+    @projects = @projects.inactive if params[:status] == 'inactive'
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.all
+    @update = Update.new
+    @updates = Update.all
+  end
+
+  def new
+    @project = Project.new
+    @project.rewards.build
+  end
+
+  def create
+    @project = Project.new
+    @project.title = params[:project][:title]
+    @project.description = params[:project][:description]
+    @project.goal = params[:project][:goal]
+    @project.start_date = params[:project][:start_date]
+    @project.end_date = params[:project][:end_date]
+    @project.image = params[:project][:image]
+    @project.category_id = params[:project][:category_id]
+    @project.user = current_user
+
+    if @project.save
+      redirect_to projects_url
+    else
+      render :new
+    end
+   end
+
+   private
+
+    def claimed_summary
+      Claim.where(project_id: @project.id).count
+    end
+
+    helper_method :claimed_summary
 end
