b1aeb080f0bc6e56a071c00efb45907d8eabd539
Lan Phan <lanc_phan@hotmail.com>
2018-01-07 13:10:27 -0500

    add pledge associations and test

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,2 +1,14 @@
 class Pledge < ApplicationRecord
+  belongs_to :user
+  belongs_to :project
+
+  validates :dollar_amount, presence: true
+  validates :user, presence: true
+  validate :owner_cannot_back_own_project
+
+  def owner_cannot_back_own_project
+    if self.user == self.project.user
+      errors.add(:user, 'Owner cannot pledge to their project')
+    end
+  end
 end
--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -4,4 +4,29 @@ class PledgeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'A pledge can be created' do
+    project = build(:project)
+    user = build(:user)
+    pledge = build(:pledge)
+    pledge.save
+
+    assert pledge.valid?
+    assert pledge.persisted?
+  end
+
+  test 'owner cannot back own project' do
+    project = build(:project)
+    pledge = build(:pledge, project: project)
+    pledge.user = project.user
+
+    assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
+  end
+
+  test 'pledge requires dollar_amount' do
+    pledge = build(:pledge, dollar_amount: nil)
+    pledge.save
+
+    assert pledge.invalid?, 'Pledge requires dollar_amount'
+  end
 end
