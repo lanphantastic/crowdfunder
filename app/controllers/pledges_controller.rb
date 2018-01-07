09e0b7b81f23368a84710cdf5b53dcd71f305de9
Lan Phan <lanc_phan@hotmail.com>
2018-01-07 14:49:32 -0500

    pledges controller

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,18 @@
+class PledgesController < ApplicationController
+  before_action :require_login
+
+  def create
+    @project = Project.find(params[:project_id])
+
+    @pledge = @project.pledges.build
+    @pledge.dollar_amount = params[:pledge][:dollar_amount]
+    @pledge.user = current_user
+
+    if @pledge.save
+      redirect_to project_url(@project), notice: "You have successfully backed #{@project.title}!"
+    else
+      flash[:alert] = @pledge.errors.full_messages.first
+      redirect_to project_url(@project)
+    end
+  end
+end
