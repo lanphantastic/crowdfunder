f671448dbf7dd30917991254e1dfdf398d69b171
Lan Phan <lanc_phan@hotmail.com>
2018-01-07 17:46:02 -0500

    add associations and validations for comment and reward

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :project
+
+  validates :text, presence: true
+  validate :user_has_pledged_project
+
+  def user_has_pledged_project
+    unless project.users.include?(user)
+      errors.add(:base, "You can't make a comment if you haven't pledged to this project.")
+    end
+  end
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,2 +1,6 @@
 class Reward < ApplicationRecord
+  belongs_to :project
+
+  validates :description, :dollar_amount, presence: true
+  validates :dollar_amount, numericality: { greater_than_or_equal_to: 0 }
 end
