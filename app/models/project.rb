454e333ec011aa445fa4a963d7a290d7869ba303
Lan Phan <lanc_phan@hotmail.com>
2018-01-07 13:05:53 -0500

    add associations and validations to user and project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,13 @@
 class Project < ApplicationRecord
+  has_many :rewards
+  has_many :pledges
+  has_many :users, through: :pledges # backers
+  has_many :updates
+  belongs_to :user # project owner
+  belongs_to :category
+  has_many :claims
+
+  has_many :comments
+  validates :title, :user_id, :description, :goal, :start_date, :end_date, presence: true
+  validates :goal, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  # associations
+  has_many :pledges
+  has_many :projects
+  has_many :pledges_projects, through: pledges, source: :project
+  has_many :comments
+
+  validates :password, length: { minimum: 8 }, on: :create
+  validates :password, confirmation: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
+
+  validates :email, uniqueness: true
 end
